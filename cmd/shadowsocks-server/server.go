6703041792eb50ed8087292df043c0fe76377285
lucus <lixin9311@gmail.com>
2017-01-25 03:57:50 +0900

    Remove some unused code

--- cmd/shadowsocks-server/server.go
+++ cmd/shadowsocks-server/server.go
@@ -18,22 +18,6 @@ import (
 	ss "github.com/shadowsocks/shadowsocks-go/shadowsocks"
 )
 
-const (
-	idType  = 0 // address type index
-	idIP0   = 1 // ip addres start index
-	idDmLen = 1 // domain address length index
-	idDm0   = 2 // domain address start index
-
-	typeIPv4 = 1 // type is ipv4 address
-	typeDm   = 3 // type is domain address
-	typeIPv6 = 4 // type is ipv6 address
-
-	lenIPv4     = net.IPv4len + 2 // ipv4 + 2port
-	lenIPv6     = net.IPv6len + 2 // ipv6 + 2port
-	lenDmBase   = 2               // 1addrLen + 2port, plus addrLen
-	lenHmacSha1 = 10
-)
-
 var debug ss.DebugLog
 var udp bool
 
