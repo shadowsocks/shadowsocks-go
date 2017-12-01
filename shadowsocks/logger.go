e1b05e2921dd503f6adfa256cf343223ff61bcf1
Gordon Yau <qunxyz@gmail.com>
2017-12-01 12:15:45 +0800

    added line and file info to logger

--- shadowsocks/logger.go
+++ shadowsocks/logger.go
@@ -33,8 +33,12 @@ func (this *LogType) Fields(fields LogFields) (*LogType) {
 }
 
 func (this *LogType) setFields() *logrus.Entry {
-	fmt.Println(this.getCaller(5) + "=>")
-	fmt.Println("  " + this.getCaller(4) + ":")
+	file, line, fun := this.getCallerInfo(5)
+	fmt.Printf("\nLine: %d %s %s =>\n", line, fun, file)
+
+	file, line, fun = this.getCallerInfo(4)
+	fmt.Printf("  Line: %d %s %s =>\n", line, fun, file)
+
 	fields := this.fields
 	this.fields = nil
 	return this.logger.WithFields((map[string]interface{})(fields))
@@ -138,21 +142,27 @@ func (this *LogType) Println(args ...interface{}) (*LogType) {
 	return this
 }
 
-func (this *LogType) getCaller(skip int) string {
+func (this *LogType) getCallerInfo(skip int) (file string, line int, fun_name string) {
 	// we get the callers as uintptrs - but we just need 1
 	fpcs := make([]uintptr, 1)
 
 	// skip 3 levels to get to the caller of whoever called Caller()
 	n := runtime.Callers(skip, fpcs)
 	if n == 0 {
-		return "n/a" // proper error her would be better
+		fun_name = "n/a"
+		return
 	}
 
 	// get the info of the actual function that's in the pointer
 	fun := runtime.FuncForPC(fpcs[0]-1)
 	if fun == nil {
-		return "n/a"
+		fun_name = "n/a"
+		return
 	}
 
-	return fun.Name()
+	fun_name = fun.Name()
+
+	file, line = fun.FileLine(fpcs[0]-1)
+
+	return
 }
